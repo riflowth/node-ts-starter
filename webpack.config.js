abd55918eb65010b9d16d0c268fbba1237a10ef3
riflowth <riflowth@gmail.com>
2022-02-22 01:47:57 +0700

    perf(webpack): decrease build time

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,48 @@
-import path from 'path';
-
-const __dirname = path.resolve();
+const path = require('path');
 const distPath = path.resolve(__dirname, 'dist');
+const srcPath = path.resolve(__dirname, 'src');
+
+const nodeExternals = require('webpack-node-externals');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const app = {
-  entry: './src/app.ts',
+  entry: path.resolve(srcPath, 'app.ts'),
   module: {
     rules: [
-      { test: /\.ts$/, use: 'ts-loader', exclude: '/node_modules/' }
-    ]
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        include: srcPath,
+        options: { transpileOnly: true },
+      }
+    ],
   },
-  resolve: {
-    extensions: ['.ts', '.js']
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'package.json'), to: distPath }
+      ],
+    })
+  ],
+  externalsPresets: { node: true },
+  externals: [nodeExternals()],
+  stats: {
+    preset: 'minimal',
+    assets: false,
+    modules: false,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({ extractComments: false })]
   },
   output: {
     filename: 'app.js',
-    path: distPath
-  }
+    path: distPath,
+    clean: true,
+  },
 };
 
-export default [app];
-
+module.exports = [app];
