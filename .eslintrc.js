3f57a3e4f1b08ac1211f0a35c78a327a17e81d61
riflowth <riflowth@gmail.com>
2022-05-21 17:01:37 +0700

    refactor: follow parserOptions guideline

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,31 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: __dirname,
+      }
+    }
+  },
   extends: [
     'airbnb-base',
     'airbnb-typescript/base',
   ],
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: './tsconfig.json',
-      }
-    }
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
+
   rules: {
     // allow console logging for nodejs
     'no-console': 'off',
@@ -34,7 +40,7 @@ module.exports = {
       { classes: 'always' },
     ],
 
-    // except empty line after atrribute scope variable declarations
+    // except empty line after attribute scope variable declarations
     '@typescript-eslint/lines-between-class-members': [
       'error',
       'always',
